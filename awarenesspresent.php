e9bf6dcb0ab7c999990e5b5b7296bf6f43985da0
root <root@v.home>
2016-02-05 00:35:39 -0500

    added first files

---
+++ awarenesspresent.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Project Awareness</title>
+    </head>
+    <body>
+        Alerts For <?php echo $_GET["county"]?><br>
+        Alerts For <?php echo $_GET["city"]?><br>
+        Alerts For <?php echo $_GET["zip"]?><br>
+    </body>
+</html>
