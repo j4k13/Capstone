e3639d930c7a160e283c5eadca055680b5e5ec21
j4k13 <namlesscrusader@gmail.com>
2016-04-04 21:33:47 -0400

    adding page puller and files

---
+++ pagepuller.php
@@ -0,0 +1,33 @@
+<?php
+	//set timezone because OMG warn'ins
+	date_default_timezone_set('America/New_York');
+	//get hash from last check
+	$lastcheck = md5_file("weather.txt");
+	//initialize curl for weather page
+	$weatherpage = curl_init("http://alerts.weather.gov/cap/va.php?x=1");
+	//clean out temp
+	
+	//open file to write
+	$weathertxt = fopen("temp.txt","w");
+	//take hash of incoming data
+	$newdata = md5_file("temp.txt");
+	//set curl to write file
+	curl_setopt($weatherpage, CURLOPT_FILE, $weathertxt);
+	//curl_setopt($weatherpage, CURLOPT_HEADER, 0);
+	//execute page pull and write
+	curl_exec($weatherpage);
+	//close curl session
+	curl_close($weatherpage);
+	//compare new page to back up
+	if($lastcheck != $newdata)
+	{
+		//load changes to weather.txt
+		$current = file_get_contents("weather.txt");
+		$new = file_get_contents("temp.txt");
+		file_put_contents($current,$new);
+		//if different push updates
+		
+	}
+	//close file	
+	fclose($weathertxt);
+?>
